191b43cac2ae37c52030bd55af65829f3d115c4c
Luca Rodrigues <luca.rodrigues@wundermanthompson.com>
2023-10-20 00:45:53 -0300

    feat: add cors and more validations

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
 const routes = require('./routes');
+const cors = require('./middlewares/cors');
+const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
-
-app.use((error, request, response, next) => {
-  response.sendStatus(500);
-});
+app.use(errorHandler);
 
 // eslint-disable-next-line no-console
 app.listen(3001, () => console.log('🔥 Server started at http://localhost:3001'));
