06ff2523e13a82c7a9c73f54a6bed044d7575cfa
Luca Rodrigues <luca.rodrigues@wundermanthompson.com>
2023-10-19 23:09:01 -0300

    feat: add contact controller and repository, its methods and base sql schema

---
+++ src/app/repositories/ContactsRepository.js
@@ -0,0 +1,74 @@
+const { v4: uuid } = require('uuid');
+
+let contacts = [
+  {
+    id: uuid(),
+    name: 'Luca',
+    email: 'luca@mail.com',
+    phone: '123123123',
+    category_id: uuid(),
+  },
+];
+
+class ContactsRepository {
+  findAll() {
+    return new Promise((resolve) => {
+      resolve(contacts);
+    });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.id === id));
+    });
+  }
+
+  findByEmail(email) {
+    return new Promise((resolve) => {
+      resolve(contacts.find((contact) => contact.email === email));
+    });
+  }
+
+  create({
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const newContact = {
+        id: uuid(),
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts.push(newContact);
+      resolve(newContact);
+    });
+  }
+
+  update(id, {
+    name, email, phone, category_id,
+  }) {
+    return new Promise((resolve) => {
+      const updatedContact = {
+        id,
+        name,
+        email,
+        phone,
+        category_id,
+      };
+      contacts = contacts.map((contact) => (
+        contact.id === id ? updatedContact : contact
+      ));
+      resolve(updatedContact);
+    });
+  }
+
+  delete(id) {
+    return new Promise((resolve) => {
+      contacts = contacts.filter((contact) => contact.id !== id);
+      resolve();
+    });
+  }
+}
+
+module.exports = new ContactsRepository();
