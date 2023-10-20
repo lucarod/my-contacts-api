077f6c1af785807472e7c3fb3c7cada173b208e9
Luca Rodrigues <luca.rodrigues@wundermanthompson.com>
2023-10-19 23:55:04 -0300

    feat: create categories module and add error treatment

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,5 +26,6 @@ module.exports = {
     'import/no-extraneous-dependencies': 'off',
     'consistent-return': 'off',
     camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
   },
 };
--- src/index.js
+++ src/index.js
@@ -6,4 +6,9 @@ const app = express();
 
 app.use(routes);
 
+app.use((error, request, response, next) => {
+  response.sendStatus(500);
+});
+
+// eslint-disable-next-line no-console
 app.listen(3001, () => console.log('🔥 Server started at http://localhost:3001'));
